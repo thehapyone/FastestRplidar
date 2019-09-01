9cb3e72f487e39bd8b067ef92e1bf07b1142c20d
Ayo Ayibiowu <charlesayibiowu@hotmail.com>
2019-09-01 15:39:36 +0200

    Create fetch_lidar_scan.py

---
+++ examples/fetch_lidar_scan.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+'''
+This code snippet demonstrate how to use the myRplidar library for 
+existing codes that depends on the RPLidar Python library (https://github.com/SkoltechRobotics/rplidar) or 
+just want to get started using this Library.
+
+
+ 
+ @authur : Ayo Ayibiowu
+ @email : charlesayibiowu@hotmail.com
+ 
+ Version: v1
+ '''
+
+from myRplidar import RPlidar
+
+# uses the default port - /dev/ttyUSB0
+lidar = RPlidar()
+
+if __name__ == '__main__':
+
+    # connects the lidar using the default port (/dev/ttyUSB0)
+    lidar.connect()
+    
+    # Get the Lidar health info
+    info = lidar.get_info()
+    print("info - ", info)
+
+    # Gets the state of the lidar health
+    health = lidar.get_health()
+    print("Health - ", health)
+    
+    # connects the lidar using the default port (tty/USB0)
+    lidar.connectlidar()
+    # Starts the lidar motor
+    lidar.startmotor()
+    
+    for i, scan in enumerate(lidar.iter_scans()):
+        print('%d: Got %d measurments' % (i, len(scan)))
+        print (scan)
+        if i > 10:
+            break
+
+    # done. Stops the motor
+    lidar.stopmotor()
+    
+    print ("done")
